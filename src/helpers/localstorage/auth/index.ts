b8f1e2145d50ece652dd8b76d952a278ec6124ce
Nicolas Carrasco <nicolas.carrasco@epitech.eu>
2021-05-04 22:09:32 +0200

    Creating localStorage + auhtToken hooks to store the jwt

---
+++ src/helpers/localstorage/auth/index.ts
@@ -0,0 +1,17 @@
+import { useLocalStorage } from '../useLocalStorage';
+
+const key = 'authToken';
+
+export const useAuthToken = (): {
+  authToken: string | null;
+  setAuthToken: (authToken: string) => void;
+  removeAuthToken: (key: string) => void;
+} => {
+  const [authToken, setValue, removeValue] = useLocalStorage<string>(key);
+
+  const setAuthToken = (token: string) => setValue(token);
+
+  const removeAuthToken = () => removeValue(key);
+
+  return { authToken, setAuthToken, removeAuthToken };
+};
