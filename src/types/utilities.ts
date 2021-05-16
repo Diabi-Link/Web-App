dec500f2d8275b69469cb01344dbdae201004d98
Nicolas Carrasco <nicolas.carrasco@epitech.eu>
2021-05-16 21:29:00 +0200

    Adding Typescript utilities

---
+++ src/types/utilities.ts
@@ -0,0 +1,27 @@
+export type Nullable<T> = T | null;
+
+export type DeepNullable<T extends object> = {
+  [K in keyof T]: Nullable<T[K]>;
+};
+
+export type RequiredExcept<T, TExcept extends keyof T> = Required<
+  DeepNonNullable<Omit<T, TExcept>>
+> &
+  Pick<T, TExcept>;
+
+// From https://github.com/piotrwitek/utility-types
+// eslint-disable @typescript-eslint/no-empty-interface
+export type DeepNonNullable<T> = T extends (...args: any[]) => any
+  ? T
+  : T extends any[]
+  ? _DeepNonNullableArray<T[number]>
+  : T extends object
+  ? _DeepNonNullableObject<T>
+  : T;
+/** @private */
+// tslint:disable-next-line:class-name
+export type _DeepNonNullableArray<T> = Array<DeepNonNullable<NonNullable<T>>>;
+/** @private */
+export type _DeepNonNullableObject<T> = {
+  [P in keyof T]-?: DeepNonNullable<NonNullable<T[P]>>;
+};
