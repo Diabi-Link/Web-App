fa7801878db6f52046b1ec317845d13dc996fdaf
Nicolas Carrasco <nicolas.carrasco@epitech.eu>
2021-05-20 03:06:04 +0200

    check if useIsAuthenticated thanks to customHooks

--- src/helpers/customHooks.ts
+++ src/helpers/customHooks.ts
@@ -1,4 +1,5 @@
 import { useRef, useEffect } from 'react';
+import { useAuthToken } from './localstorage/auth';
 
 // Use it on useEffect props dependencies which are object or array to...
 // ...trigger the useEffect only when the content of these props change
@@ -15,4 +16,16 @@ export function usePrevious<T>(value: T): T {
 
   // Return previous value (happens before update in useEffect above)
   return ref.current;
+}
+
+export function useIsAuthenticated(): boolean {
+  const { authToken } = useAuthToken();
+
+  if (!authToken) {
+    return false;
+  }
+
+  return true;
+
+  // TODO: return true only if user !== null OR if fetching user with a request and the accessToken and refreshToken
 }
