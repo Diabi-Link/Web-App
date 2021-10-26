7724c39228c7bca5cf6ddd8298053e625de9903e
Nicolas Carrasco <nicolas.carrasco@epitech.eu>
2021-10-26 15:15:50 +0200

    Add page title

--- src/Pages/AuthApp/AddMeasurement/index.tsx
+++ src/Pages/AuthApp/AddMeasurement/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import Heading from '../../../ui/Heading';
 
 const Container = styled.div`
   display: flex;
@@ -7,8 +8,51 @@ const Container = styled.div`
   align-items: center;
 `;
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 80vw;
+`;
+
+const PageTitle = styled(Heading)`
+  position: relative;
+  color: ${({ theme }) => theme.main.primaryLight};
+  margin-top: 2rem;
+
+  &:before,
+  &:after {
+    content: '';
+    height: 10%;
+    top: 50%;
+    position: absolute;
+  }
+
+  &:before {
+    background-color: ${({ theme }) => theme.main.whiteBroken};
+    left: -1.5em;
+    right: -1.5em;
+    z-index: -1;
+  }
+
+  &:after {
+    left: 50%;
+    transform: translateX(-50%);
+    width: 80vw;
+    z-index: -2;
+    background-color: ${({ theme }) => theme.main.primaryLight};
+  }
+`;
+
 const AddData = () => {
-  return <Container>salut</Container>;
+  return (
+    <Container>
+      <Wrapper>
+        <PageTitle level={2}>Ajouter une mesure</PageTitle>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default AddData;
