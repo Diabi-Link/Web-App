4624676988132a99063a1ce432a342412a3a7df5
Nicolas Carrasco <nicolas.carrasco@epitech.eu>
2021-10-26 22:46:36 +0200

    Error handling on input for big and small numbers

--- src/Pages/AuthApp/AddMeasurement/Validation/index.tsx
+++ src/Pages/AuthApp/AddMeasurement/Validation/index.tsx
@@ -1,7 +1,9 @@
 import * as Yup from 'yup';
 
 const ValidateProfileSchema = Yup.object().shape({
-  bloodSugarLevels: Yup.number(),
+  bloodSugarLevels: Yup.number()
+    .min(0.1, 'La mesure doit être au minimum à 0.10 g/L')
+    .max(5.0, 'La mesure doit être au maxium à 5.00 g/L'),
 });
 
 export { ValidateProfileSchema };
