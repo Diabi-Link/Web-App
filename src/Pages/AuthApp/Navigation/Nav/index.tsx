2c85fa32de8f84a304ce0194a980440208f55653
Nicolas Carrasco <nicolas.carrasco@epitech.eu>
2021-12-06 00:44:07 +0100

    Create temp page for graph

--- src/Pages/AuthApp/Navigation/Nav/index.tsx
+++ src/Pages/AuthApp/Navigation/Nav/index.tsx
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Loader from '../../../../ui/Loader';
 import NavigationWrapper from '../NavigationWrapper';
+import TimeInTargetGraph from '../../Analytics/TimeInTargetGraph';
+import DailyGraph from '../../Analytics/DailyGraph';
 
 const Home = lazy(() => import('../../Home'));
 const Profile = lazy(() => import('../../Profile'));
@@ -31,11 +33,49 @@ const Nav = (): JSX.Element => {
           <Route path="/add-measurement" exact component={AddMeasurement} />
           <Route path="/analytics" exact component={Analytics} />
           <Route path="/profile" exact component={Profile} />
+          <Route
+            path="/time"
+            exact
+            render={() => (
+              <Container>
+                <Wrapper2>
+                  <TimeInTargetGraph />
+                </Wrapper2>
+              </Container>
+            )}
+          />
+          <Route
+            path="/daily"
+            exact
+            render={() => (
+              <Container>
+                <Wrapper2>
+                  <DailyGraph />
+                </Wrapper2>
+              </Container>
+            )}
+          />
           <Redirect to="/" />
         </Switch>
       </Suspense>
     </NavigationWrapper>
   );
 };
+
+const Container = styled.div`
+  height: 100vh;
+  width: auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Wrapper2 = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 80vw;
+`;
 
 export default Nav;
