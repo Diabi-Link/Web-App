9d8aa1d7d5bebd978b02d43048ba8de9b1e3ad92
Nicolas Carrasco <nicolas.carrasco@epitech.eu>
2022-05-08 18:52:45 +0200

    Add Deconnexion button

--- src/Pages/AuthApp/Home/index.tsx
+++ src/Pages/AuthApp/Home/index.tsx
@@ -1,13 +1,7 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
-import { info as InfoIcon } from 'react-icons-kit/icomoon/info';
-import { useAuthToken } from '../../../hooks/useAuthToken';
 
-import { UserActionTypes, UserContext } from '../../../contexts/UserContext';
-import { ContextActionTypes, MainContext } from '../../../contexts/MainContext';
-
-import Button from '../../../ui/Button';
+import { UserContext } from '../../../contexts/UserContext';
 
 const Container = styled.div`
   height: 100vh;
@@ -26,61 +20,19 @@ const Wrapper = styled.div`
 
 const Text = styled.p``;
 
-const LogoutButton = styled(Button)`
-  margin-top: 2rem;
-`;
-
 const Home = (): React.ReactElement => {
   const {
     state: { user },
-    dispatch,
   } = useContext(UserContext);
-  const { dispatch: altDispatch } = useContext(MainContext);
-  const { removeAuthToken, setAuthToken } = useAuthToken();
-
-  useEffect(() => {
-    altDispatch({
-      type: ContextActionTypes.SetNotice,
-      payload: {
-        label: 'Cette page est temporaire',
-        icon: InfoIcon,
-        noticeStyle: 'info',
-        persistent: true,
-        closeable: true,
-        duration: 0,
-      },
-    });
-  }, []);
-  const logout = (): void => {
-    removeAuthToken();
-    dispatch({ type: UserActionTypes.EmptyUser });
-  };
-
-  const birthDate = user?.birthDate as Date;
 
   return (
     <Container data-testid="auth-home-page">
       <Wrapper>
         <Text>
-          {user?.firstName} {user?.lastName.toUpperCase()} est bien authentifié
-          !
+          Bienvenue sur DiabiLink {user?.firstName}{' '}
+          {user?.lastName.toUpperCase()} !
         </Text>
         <Text>Type de compte : {user?.account}</Text>
-        <Text>
-          Date de naissance :{' '}
-          {`${birthDate.getFullYear()}-${birthDate.getMonth()}-${birthDate.getDay()}`}
-        </Text>
-        <LogoutButton
-          label="Se déconnecter"
-          btnStyle="primary"
-          onClick={logout}
-          data-testid="logout-button"
-        />
-        <LogoutButton
-          label="Mettre un token invalide"
-          btnStyle="primary"
-          onClick={() => setAuthToken('test')}
-        />
       </Wrapper>
     </Container>
   );
